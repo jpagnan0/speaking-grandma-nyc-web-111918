f8daefc57641ddaf54cd06cc1367e83e4f6f7ad0
Adam Saland <a.jpagnano@gmail.com>
2018-11-08 01:02:42 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,6 @@
 # Write a speak_to_grandma method.
-def speak_to_grandma()
+def speak_to_grandma(say="HUH?! SPEAK UP, SONNY!")
+  if
 end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
